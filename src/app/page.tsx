a744cccfa25503abd29fc7cfc4c92df053c84dc9
buodunoyakata <budounoyakata@gmail.com>
2025-04-30 17:49:39 +0900

    特になし

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,28 +24,53 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const flipStones = (x: number, y: number, newBoard: number[][]) => {
+    let isValidMove = false;
 
-  const clickHander = (x: number, y: number) => {
+    direction.forEach(([dx, dy]) => {
+      let nx = x + dx;
+      let ny = y + dy;
+      const stonesToFlip = [];
+
+      while (ny >= 0 && ny < 8 && nx >= 0 && nx < 8 && newBoard[ny][nx] === 3 - turnColor) {
+        stonesToFlip.push([nx, ny]);
+        nx += dx;
+        ny += dy;
+      }
+
+      if (ny >= 0 && ny < 8 && nx >= 0 && nx < 8 && newBoard[ny][nx] === turnColor) {
+        isValidMove = true;
+        stonesToFlip.forEach(([fx, fy]) => {
+          newBoard[fy][fx] = turnColor;
+        });
+      }
+    });
+
+    return isValidMove;
+  };
+
+  const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
-    if (
-      (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) ||
-      (board[y - 1] !== undefined && board[y - 1][x] === 3 - turnColor) ||
-      (board[x + 1] !== undefined && board[y][x + 1] === 3 - turnColor) ||
-      (board[x - 1] !== undefined && board[y][x - 1] === 3 - turnColor)
-    ) {
-      newBoard[y][x] = turnColor;
+    if (flipStones(x, y, newBoard)) {
       newBoard[y][x] = turnColor;
       setBoard(newBoard);
       setTurnColor(3 - turnColor);
-    } else print();
+    } else {
+      console.log('ここには置けません！');
+    }
+    {
+      newBoard[y][x] = turnColor;
+      setBoard(newBoard);
+      setTurnColor(3 - turnColor);
+    }
   };
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
-            <div key={`${x}-${y}`} onClick={() => clickHander(x, y)} className={styles.cell}>
+            <div key={`${x}-${y}`} onClick={() => clickHandler(x, y)} className={styles.cell}>
               {color !== 0 && (
                 <div
                   className={styles.stone}
