8e13a6efd365a466a555012c7e9d20e7b8870460
buodunoyakata <budounoyakata@gmail.com>
2025-05-01 20:26:46 +0900

    ifでやりきる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export const direction = [
   [0, -1],
   [-1, -1],
 ];
+
 export default function Home() {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
@@ -24,42 +25,20 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const flipStones = (x: number, y: number, newBoard: number[][]) => {
-    let isValidMove = false;
-
-    direction.forEach(([dx, dy]) => {
-      let nx = x + dx;
-      let ny = y + dy;
-      const stonesToFlip = [];
-
-      while (ny >= 0 && ny < 8 && nx >= 0 && nx < 8 && newBoard[ny][nx] === 3 - turnColor) {
-        stonesToFlip.push([nx, ny]);
-        nx += dx;
-        ny += dy;
-      }
-
-      if (ny >= 0 && ny < 8 && nx >= 0 && nx < 8 && newBoard[ny][nx] === turnColor) {
-        isValidMove = true;
-        stonesToFlip.forEach(([fx, fy]) => {
-          newBoard[fy][fx] = turnColor;
-        });
-      }
-    });
-
-    return isValidMove;
-  };
-
-  const clickHandler = (x: number, y: number) => {
+  const clickHander = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
-    if (flipStones(x, y, newBoard)) {
+    if (
+      (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) ||
+      (board[y - 1] !== undefined && board[y - 1][x] === 3 - turnColor) ||
+      (board[x + 1] !== undefined && board[y][x + 1] === 3 - turnColor) ||
+      (board[x - 1] !== undefined && board[y][x - 1] === 3 - turnColor) ||
+      (board[y + 1][x + 1] !== undefined && board[y + 1][x + 1] === 3 - turnColor) ||
+      (board[y + 1][x - 1] !== undefined && board[y + 1][x - 1] === 3 - turnColor) ||
+      (board[y + 1][x] !== undefined && board[y + 1][x] === 3 - turnColor) ||
+      (board[y + 1][x] !== undefined && board[y + 1][x] === 3 - turnColor)
+    ) {
       newBoard[y][x] = turnColor;
-      setBoard(newBoard);
-      setTurnColor(3 - turnColor);
-    } else {
-      console.log('ここには置けません！');
-    }
-    {
       newBoard[y][x] = turnColor;
       setBoard(newBoard);
       setTurnColor(3 - turnColor);
@@ -70,7 +49,7 @@ export default function Home() {
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
-            <div key={`${x}-${y}`} onClick={() => clickHandler(x, y)} className={styles.cell}>
+            <div key={`${x}-${y}`} onClick={() => clickHander(x, y)} className={styles.cell}>
               {color !== 0 && (
                 <div
                   className={styles.stone}
